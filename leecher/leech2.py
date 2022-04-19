614c1b4544c046deca5e4554be9359ae6b1308a1
Harish Reddy <harish0796@gmail.com>
2022-04-19 15:07:01 -0400

    modified leech script to only add 100 problems

--- leecher/leech2.py
+++ leecher/leech2.py
@@ -3,14 +3,17 @@
 import json, requests
 
 data = json.load(open("data.json", 'r'))
-
+j = 0
 for i in range(len(data)):
     if data[i]["content"] is None:
         continue
     
     if data[i]["solution"]:
         data[i]["solution"] = data[i]["solution"]["content"]
-    
-    r = requests.post('http://localhost:8080/api/v1/problems', json=data[i])
-    print(i)
+
+        r = requests.post('http://localhost:8080/api/v1/problems', json=data[i])
+        print(i)
+        j+=1
+    if j == 100:
+        break
     # print(f"Status Code: {r.status_code}, Response: {r.json()}")
